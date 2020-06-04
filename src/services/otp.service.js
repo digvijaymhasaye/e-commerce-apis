257fff518a10fcea3f84df504c369a69664b8eb9
Digvijay Mhasaye <digvijay.mhasaye@yapsody.com>
2020-06-04 14:44:10 +0530

    commented msg91 api call

--- src/services/otp.service.js
+++ src/services/otp.service.js
@@ -54,10 +54,10 @@ const send = async ({ account_id, mobile_number }) => {
     errorUtils.throwPreconditionFailed('OTP already sent');
   }
 
-  const url = `https://api.msg91.com/api/v5/otp?authkey=${config.MSG91_AUTH_KEY}&template_id=${config.MSG91_OTP_TEMPLATE_ID}&mobile=91${mobile_number}
-    &invisible=1&otp_expiry=5`;
+  // const url = `https://api.msg91.com/api/v5/otp?authkey=${config.MSG91_AUTH_KEY}&template_id=${config.MSG91_OTP_TEMPLATE_ID}&mobile=91${mobile_number}
+  //   &invisible=1&otp_expiry=5`;
 
-  await axios.get(url);
+  // await axios.get(url);
 
   return OtpModel.create({
     account_id,
@@ -73,18 +73,18 @@ const resend = async ({ account_id, mobile_number }) => {
     errorUtils.throwPreconditionFailed('OTP already sent. It can take upto 2 minutes to get deliver OTP.');
   }
 
-  const url = `https://api.msg91.com/api/v5/otp/retry?mobile=91${mobile_number}&authkey=${config.MSG91_AUTH_KEY}&retrytype=text`;
+  // const url = `https://api.msg91.com/api/v5/otp/retry?mobile=91${mobile_number}&authkey=${config.MSG91_AUTH_KEY}&retrytype=text`;
 
-  await axios.post(url);
+  // await axios.post(url);
 
   return updateStatusByMobileNo({ account_id, mobile_no: mobile_number, status: OTP_STATUS.RESENT });
 };
 
 const verify = async ({ account_id, mobile_number, otp }) => {
   try {
-    const url = `https://api.msg91.com/api/v5/otp/verify?mobile=91${mobile_number}&otp=${otp}&authkey=${config.MSG91_AUTH_KEY}`;
+    // const url = `https://api.msg91.com/api/v5/otp/verify?mobile=91${mobile_number}&otp=${otp}&authkey=${config.MSG91_AUTH_KEY}`;
 
-    await axios.post(url);
+    // await axios.post(url);
   } catch (error) {
     if (error.message === 'otp_expired') {
       await OtpModel.update({
