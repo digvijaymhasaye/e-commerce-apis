c986f33e9b90dfd8a7cd4ac12d4ca08e58388f45
Digvijay Mhasaye <digvijay.mhasaye@yapsody.com>
2020-06-14 09:49:16 +0530

    updated address route

--- src/routes/customer-address.routes.js
+++ src/routes/customer-address.routes.js
@@ -4,8 +4,8 @@ const { customerAddressController } = require('../controllers');
 const customerAddressRoutes = express.Router({ mergeParams: true });
 
 // customerAddressRoutes.get('/count', customerAddressController.getAddressCount);
-customerAddressRoutes.get('/', customerAddressController.getAddressList);
-customerAddressRoutes.get('/:addressId', customerAddressController.getAddress);
+customerAddressRoutes.get('/', customerAddressController.getAddress);
+// customerAddressRoutes.get('/:addressId', customerAddressController.getAddress);
 customerAddressRoutes.post('/', customerAddressController.addAddress);
 // customerAddressRoutes.put('/:addressId', customerAddressController.updateAddress);
 
